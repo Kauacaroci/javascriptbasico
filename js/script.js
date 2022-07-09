e4a400ebfd103af186a572c2049c89e5770360bd
Gerso <101677208+Kauacaroci@users.noreply.github.com>
2022-07-08 22:25:16 -0300

    louder adicioned

--- js/script.js
+++ js/script.js
@@ -1,36 +1,129 @@
-//função q realiza a soma de 2 numeros
+// loader
+// funções que sao executadas quando a pagina é eecutada
+
+document.addEventListener('DOMContentLoaded',function(){
+    ocultaLoader()
+
+})
+// função que exibe o loader
+
+const exibeLoader = () => {
+    $('.preloader').fadeTo("slow", 1);
+}
+
+const ocultaLoader = () => {
+
+    $('.preloader').fadeOut("slow", 0);
+}
+ 
+
+
+
+
+
+
+
+
+// função que realiza a soma de dois numeros
+
 
 const soma = () => {
+    // declara uma variável e atribui valor
+    let numero1 = window.prompt("Digite o primeiro número");
+  
+    let numero2 = prompt("Digite o segundo número");
+  
+    console.log(numero1);
+    console.log(numero2);
+  
+    let soma = parseFloat(numero1) + parseFloat(numero2);
+  
+    console.log(soma);
+  
+    // maneiras de visualizar o que está na variável na tela
+    // alert('A soma dos números:'+soma)
+    alert(`A soma dos números: ${soma}`);
+  };
+  
+  const soma2 = (numero1, numero2) => {
+    let soma = numero1 + numero2;
+    alert(soma);
+  };
+  
+  // Função que soma 2 números digitados pelo usuário em um form
+  const somaNumeros = () => {
+    // cria uma variável e captura o valor do input com id = numero1
+    let numero1 = document.getElementById("numero1").value;
+  
+    let numero2 = document.getElementById("numero2").value;
+  
+    let soma = parseFloat(numero1) + parseFloat(numero2);
+  
+    // Exibe a soma dentro da div resultado
+    document.getElementById("resultado").innerHTML = `
+          Resultado da soma é ${soma}
+      `;
+  };
+  
+  const exibe = (id) => {
+    let elemento = document.getElementById(id);
+  
+    if (elemento.hidden == true) {
+      elemento.hidden = false;
+    } else {
+      elemento.hidden = true;
+    }
+  };
+  
+  const exibeCidade = () => {
+    // captura o elemento html do estado
+    let estado = document.getElementById("estado");
+    let cidade = document.getElementById("cidade");
+  
+    // exibe o valor do elemento capturado
+    // alert(estado.value)
+  
+    if (estado.value == "sp") {
+      cidade.innerHTML = `
+      <option value="">São João da Boa Vista</option>
+      <option value="">Aguaí</option>
+      <option value="">Águas da Prata</option>
+      `;
+    } else if (estado.value == "rj") {
+      cidade.innerHTML = `
+      <option value="">Laranjeiras</option>
+      <option value="">Xerém</option>
+      <option value="">Parati</option>
+      `;
+    } else if (estado.value == "mg") {
+      cidade.innerHTML = `
+      <option value="">Andradas</option>
+      <option value="">Poços de Caldas</option>
+      <option value="">Belo Horizonte</option>
+      `;
+    }
+  };
+  
+  // função que consulta o cep de uma API
+  // api utilizando POSTMON
+  const consultaCep = () =>{
+  
+      let cep = document.getElementById('cep').value
+    exibeLoader();
+     const result = fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        .then(response=>response.json())
+        .then(result=>{
+            // resposta final da requisitação, ja validada em formato JSON
+            // manipulação do HTML
+           //alert(result.logradouro)
+            document.getElementById('logradouro').value = result.logradouro
+            document.getElementById('bairro').value = result.bairro
+            document.getElementById('localidade').value = result.localidade
+            document.getElementById('uf').value = result.uf
 
-    // declara uma variavel e atribui valor
-let numero1 = window.prompt('Digite o primeiro numero')
+            document.getElementById('numero').focus();
 
-let numero2 = window.prompt('Digite o segundo numero')
 
-//parsseint converte uma string em inteiro e parsefloat para dicimal
-
-let soma = parseFloat(numero1) + parseFloat(numero2)
-
-alert('Soma dos numeros: '+soma)
-
-}
-
-const soma2 = (numero1,numero2) => {
-    let soma = numero1 + numero2
-    alert(soma)
-}
-
-//Função que soma os 2 numeros digitados pelo usuario em um ''formulario''
-const somaNumeros = () =>{
-    //Cria uma variavel e captura o valor do input com id = numero1
-    let numero1 = document.getElementById('numero1').value
-
-    let numero2 = document.getElementById('numero2').value
-
-    let soma = parseFloat(numero1) + parseFloat(numero2)
-
-    // exibe a soma dentro da div resultado
-    document.getElementById('resultado').innerHTML =`
-    Resultado da soma é ${soma}`
-
-}
+    ocultaLoader();
+        })
+  }
